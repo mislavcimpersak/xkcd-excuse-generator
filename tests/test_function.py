f675565c27fa3851d4bcfc6c2cd87b83a9fad92a
Mislav Cimperšak <mislav.cimpersak@styria.hr>
2017-10-08 16:12:37 +0200

    test for input sanitazion

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,2 +1,31 @@
-def test_dummy():
-    assert 1 + 1 == 2
+from app import _sanitize_input
+
+
+def test_sanitize_input__simple():
+    INPUT = 'programmer'
+    OUTPUT = 'PROGRAMMER'
+    assert _sanitize_input(INPUT) == OUTPUT
+
+
+def test_sanitize_input__non_ascii():
+    INPUT = 'čovjek'
+    OUTPUT = 'COVJEK'
+    assert _sanitize_input(INPUT) == OUTPUT
+
+
+def test_sanitize_input__extra_spacing():
+    INPUT = ' programmer  '
+    OUTPUT = 'PROGRAMMER'
+    assert _sanitize_input(INPUT) == OUTPUT
+
+
+def test_sanitize_input__sentence():
+    INPUT = 'my code is compiling'
+    OUTPUT = 'MY CODE IS COMPILING'
+    assert _sanitize_input(INPUT) == OUTPUT
+
+
+def test_sanitize_input__sentence_extra_spacing_non_ascii():
+    INPUT = '  my code is compiling čovječe'
+    OUTPUT = 'MY CODE IS COMPILING COVJECE'
+    assert _sanitize_input(INPUT) == OUTPUT
